5b0bd1972267278ca63df588e25b7db804a8e172
soyoon19 <96063941+soyoon19@users.noreply.github.com>
2024-11-04 10:41:00 +0900

    first commit

---
+++ src/main/java/com/example/TimeBrew_Server/TimeBrewServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.TimeBrew_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeBrewServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeBrewServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TimeBrew_Server/TimeBrewServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TimeBrew_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeBrewServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
